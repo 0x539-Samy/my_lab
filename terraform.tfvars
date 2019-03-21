97f2c9c744cefa8a6466e2c7d68766d6d030feff
0x539-Samy <34910278+0x539-Samy@users.noreply.github.com>
2019-03-21 23:14:52 +0100

    add new vars files for default value

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,3 @@
 aws_region = "eu-west-3"
 aws_instance_type = "t2.micro"
-aws_instance_tags = "my_aws_server"
+aws_instance_tags = "my_aws_server_01"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+    description = "AWS region to launch servers."
+}
+
+variable "aws_instance_type" {
+    description = "Instance type"
+}
+
+variable "aws_instance_tags" {
+    description = "instance tag"
+}
